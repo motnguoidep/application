c0ada0abbfe78ded1d60b772fa9a8afbce4ab144
TrongPD2 <phungductrong1999@gmail.com>
2023-10-08 21:41:25 +0700

    import tailwind css

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView'
-import MealsByIngredient from '../views/MealsByIngredients'
-import MealsByLetter from '../views/MealsByLetter'
-import MealsByName from '../views/MealsByName'
+import HomeView from '../views/HomeView.vue'
+import MealsByIngredient from '../views/MealsByIngredients.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByName from '../views/MealsByName.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
-
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
